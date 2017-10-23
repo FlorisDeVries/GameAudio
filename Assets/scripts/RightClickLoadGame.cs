2613599e1565c1ecb3f5af0678800debe99a7f36
bend-PC\bend <bendeguzszatmari@gmail.com>
2017-10-23 17:14:49 +0200

    mainmenu added

---
+++ Assets/scripts/RightClickLoadGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RightClickLoadGame : MonoBehaviour 
+{
+
+	void Update(){
+		if (Input.GetMouseButtonDown (1)) {
+			Debug.Log ("Pressed right click."); 
+			SceneManager.LoadScene ("MainGame");
+		}
+	}
+}
