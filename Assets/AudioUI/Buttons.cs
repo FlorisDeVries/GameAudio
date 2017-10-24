3435134df195a98f3c08328ffeaf871fb1905b23
FlorisDeVries <florisdev@hotmail.com>
2017-10-24 11:52:25 +0200

    More work on sound

---
+++ Assets/AudioUI/Buttons.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour{
+	
+public AudioClip hatch;
+private AudioSource audioSource;
+	 
+	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource> ();
+	}
+		
+	void Update(){
+							if (Input.GetMouseButtonDown(0))
+							Debug.Log("Pressed left click.");
+							if (Input.GetMouseButtonDown(1))
+							Debug.Log("Pressed right click.");
+
+			if (Input.GetMouseButtonDown (0)) {
+				//	Application.Quit ();
+				Debug.Log ("Quit");
+			}
+			if (Input.GetMouseButtonDown (1)) {
+				//move to MainGame	
+				audioSource.clip = hatch;
+				audioSource.Play ();
+				SceneManager.LoadScene ("MainGame");
+			}
+	}
+}	
